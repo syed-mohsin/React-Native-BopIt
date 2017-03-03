e3c82b630165f5aa407e531bb45aa67444f707c6
Jonathan Zinger <jzinger1@babson.edu>
2017-03-02 19:00:34 -0800

    #5 added scoring routes

--- express-server/models.js
+++ express-server/models.js
@@ -1,15 +1,59 @@
 var mongoose = require('mongoose');
 
 var userSchema = mongoose.Schema({
+<<<<<<< Updated upstream
   username: String,
   password: String,
   phone: String
 });
 
 
+=======
+  username: {
+    type: String,
+    index: {
+      unique: true
+    },
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  highScore: {
+    type: Number
+  },
+  numGamesPlayed: {
+    type: Number
+  },
+  timeElapsed: {
+    type: Number
+  }
+});
+
+var questionSchema = mongoose.Schema({
+  question: {
+    type: String
+  },
+  codeSnippet: {
+    type: String
+  },
+  choices: {
+    type: Array
+  },
+  correctAnswer: {
+    type: String
+  },
+  difficulty: {
+    type: String
+  }
+})
+>>>>>>> Stashed changes
 
 User = mongoose.model('User', userSchema);
+Question = mongoose.model('Question', questionSchema);
 
 module.exports = {
-    User:User
+    User: User,
+    Question: Question
 };
--- express-server/routes/routes.js
+++ express-server/routes/routes.js
@@ -1,34 +1,108 @@
 var express = require('express');
 var router = express.Router();
-var models = require('../models');
-var User = models.User;
+var models = require('../models/models');
 
-//////////////////////////////// PUBLIC ROUTES ////////////////////////////////
-// Users who are not logged in can see these routes
+module.exports = function() {
 
-router.get('/', function(req, res, next) {
-  res.render('home');
-});
+  router.post('/scores', function(req, res) {
+    models.User.findOneAndUpdate({username: req.body.username}, {highScore: req.body.score}, function(err, foundUser){
+      if(err){
+        console.log(err);
+        res.status(400).json({
+          success: false,
+          message: err.errmsg
+        });
+      } else {
+        res.status(200).json({
+          success: true,
+          message: req.body.score
+        })
+      }
+    })
+  })
 
-///////////////////////////// END OF PUBLIC ROUTES /////////////////////////////
-
-router.use(function(req, res, next){
-  if (!req.user) {
-    res.redirect('/login');
-  } else {
-    return next();
+  router.get('/scores', function(req, res) {
+    models.User.findOne({username: req.body.username}, function(err, foundUser){
+      if(err){
+        console.log(err);
+        res.status(400).json({
+          success: false,
+          message: err.errmsg
+        });
+      } else if(!foundUser.highScore){
+        res.status(200).json({
+          success: true,
+          message: "No high score."
+        });
+      } else {
+        res.status(200).json({
+          success: true,
+          message: "High score found",
+          highScore: foundUser.highScore
+        })
+      }
+    });
   }
-});
 
-//////////////////////////////// PRIVATE ROUTES ////////////////////////////////
-// Only logged in users can see these routes
+  router.get('/leaderboard', function(req, res){
+    models.User.find({}, function(err, foundUserArray){
+      if(err) {
+        console.log(err);
+        res.status(400).json({
+          success: false,
+          message: err.errmsg
+        });
+      } else if(!foundUserArray){
+        res.status(200).json({
+          success: true,
+          message: "No leaderboard."
+        });
+      } else {
+        var sortedArray = foundUserArray.slice().sort(function(a, b){
+          return a.highScore - b.highScore;
+        });
+        res.status(200).json({
+          success: true,
+          highScores: sortedArray
+        });
+      }
+    })
+  })
 
-router.get('/protected', function(req, res, next) {
-  res.render('protectedRoute', {
-    username: req.user.username,
-  });
-});
-
-///////////////////////////// END OF PRIVATE ROUTES /////////////////////////////
+  // router.post('/register', function(req, res) {
+  //   var u = new models.User({
+  //     username: req.body.username,
+  //     password: req.body.password
+  //   });
+  //   u.save(function(err, user) {
+  //     if (err) {
+  //       console.log(err);
+  //       res.status(400);
+  //       return;
+  //     }
+  //     console.log(user);
+  //     res.status(200).json(user)
+  //   });
+  // });
+  //
+  // // POST Login page
+  // router.post('/login', function(req, res) {
+  //   models.User.findOne({username: req.body.username}, function(err, foundUser){
+  //     if(err){
+  //       res.status(500);
+  //     } else if(!foundUser){
+  //       res.status(400).json({
+  //         success: false,
+  //         message: "User not found."
+  //       });
+  //     } else if(foundUser.password === req.body.password) {
+  //       res.json({
+  //         success: true,
+  //         message: "Successfully logged in."
+  //       });
+  //     }
+  //   })
+  // });
+>>>>>>> Stashed changes
 
 module.exports = router;
